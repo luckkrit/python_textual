fb82aa2eea6fadbab1e4f114ef3a33df2d7151e5
Krit <luckkrit@hotmail.com>
2026-01-12 03:43:05 +0000

    add style to textual

--- main.py
+++ main.py
@@ -1,37 +1,11 @@
-import asyncio
-from os import system, waitstatus_to_exitcode
-from textual import work
-from textual.app import App, ComposeResult
-from textual.widgets import Label,Button
+from textual.app import App
 
+class HelloStyleApp(App):
 
-class LanguageApp(App[str]):
-    
-    def compose(self)->ComposeResult:
-        yield Label("What is your favorite ide?")
-        yield Button("Nano", id="nano")
-        yield Button("Vim", id="vi")
-        yield Button("Exit", id="exit")
-    
-    @work
-    async def on_button_pressed(self, event: Button.Pressed)->None:
-        if event.button.id == "exit":
-            self.notify("Thank for use it.", timeout=5)
-            await asyncio.sleep(5)
-            self.exit(event.button.id)
-
-        with self.suspend():
-            raw_status = system(event.button.id)
-            exit_code = waitstatus_to_exitcode(raw_status)
-        
-        if exit_code == 0:
-            self.notify(f"Success: {event.button.id} closed normally.")
-            
-        else:
-            self.notify(f"Exit Code: {exit_code}", severity="error")
-
+    def on_mount(self)->None:
+        self.screen.styles.background = "darkgreen"
+        self.screen.styles.border = ("dashed", "yellow")
 
 if __name__ == "__main__":
-    app = LanguageApp()
-    return_code = app.run()
-    print(f"Your favourite ide is: {return_code}")
+    app = HelloStyleApp()
+    app.run()
