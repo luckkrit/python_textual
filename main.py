640db2cf8512ef9efd7c8b302fa004c8517c87e4
Krit <luckkrit@hotmail.com>
2026-01-12 03:52:33 +0000

    add style to widget

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from textual.app import App
+from textual.app import App, ComposeResult
+from textual.widgets import Button, Label
 
-class HelloStyleApp(App):
+class WidgetApp(App):
+
+    def compose(self) -> ComposeResult:
+        self.label = Label("Python")
+        self.button = Button("Exit")
+        yield self.label
+        yield self.button
 
     def on_mount(self)->None:
-        self.screen.styles.background = "darkgreen"
-        self.screen.styles.border = ("dashed", "yellow")
+        self.label.styles.background = "darkblue"
+        self.label.styles.border = ("heavy", "white")
+        self.button.styles.background = "red"
+    
+    def on_button_pressed(self,event: Button.Pressed)->None:
+        self.exit()
 
 if __name__ == "__main__":
-    app = HelloStyleApp()
+    app = WidgetApp()
     app.run()
